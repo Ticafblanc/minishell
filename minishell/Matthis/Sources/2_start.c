429b8a8c5ceb35933f163e62db06fbeb02e13f45
mdoquocb <mdoquocb@student.42quebec.com>
2022-06-13 14:49:13 -0400

    test

--- minishell/Matthis/Sources/2_start.c
+++ minishell/Matthis/Sources/2_start.c
@@ -19,8 +19,8 @@ int    start_minishell(t_global *global)
         global->command = readline("Minishell % ");
         if (global->command && *global->command)
             add_history(global->command);
-        if (strnstr(global->command, "clear", 6))//a ajouter a parsing
-            rl_clear_history(); 
+        //if (strnstr(global->command, "clear", 6))//a ajouter a parsing
+            //rl_clear_history(); 
         ft_putstr(global->command); //test a suprimer
         write (1, "\n", 1); // test a supprimer
         if (strnstr(global->command, "exit", 6))//a ajouter a parsing
