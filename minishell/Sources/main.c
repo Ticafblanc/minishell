c1fe5241caf93b48dd674bb2d2671c70a9bcef65
tonted <yddetco@gmail.com>
2022-09-26 17:41:54 -0400

    :recycle: Norminette in progress
    norminette in progress!

--- minishell/Sources/main.c
+++ minishell/Sources/main.c
@@ -6,39 +6,44 @@
 /*   By: tonted <tonted@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/11 18:29:46 by mdoquocb          #+#    #+#             */
-/*   Updated: 2022/09/26 16:52:06 by tonted           ###   ########.fr       */
+/*   Updated: 2022/09/26 17:39:27 by tonted           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell.h>
 
-static int	check_command(char **command, int *status)
+int	child_process(int fd[2])
 {
 	char	*str;
 
+	signal(SIGINT, handle_exec);
+	close(fd[STDIN_FILENO]);
+	str = readline("> ");
+	while (!str || !*str)
+	{
+		if (!str)
+			exit(perror_minishell(TOKENERR, "end of file"));
+		str = readline("> ");
+	}
+	ft_putstr_fd(str, fd[STDOUT_FILENO]);
+	free(str);
+	close(fd[STDOUT_FILENO]);
+	exit(EXIT_SUCCESS);
+}
+
+static int	check_command(char **command, int *status)
+{
+	char	*str;
 	pid_t	pid;
 	int		fd[2];
+
 	if (pipe(fd) == -1)
-        	return(perror_minishell(errno, "Fork child_process"));
+		return (perror_minishell(errno, "Fork child_process"));
 	pid = fork();
 	if (pid == -1)
-    	return(perror_minishell(errno, "Fork child_process"));
+		return (perror_minishell(errno, "Fork child_process"));
 	if (!pid)
-	{
-		signal(SIGINT, handle_exec);
-		close(fd[STDIN_FILENO]);
-		str = readline("> ");
-		while (!str || !*str)
-		{
-			if (!str)
-				exit(perror_minishell(TOKENERR, "end of file"));
-			str = readline("> ");
-		}
-		ft_putstr_fd(str, fd[STDOUT_FILENO]);
-		free(str);
-		close(fd[STDOUT_FILENO]);
-		exit(EXIT_SUCCESS);
-	}
+		child_process(fd);
 	close(fd[STDOUT_FILENO]);
 	waitpid(pid, status, 0);
 	if (!*status)
@@ -48,7 +53,6 @@ static int	check_command(char **command, int *status)
 	}
 	close(fd[STDIN_FILENO]);
 	return (*status);
-
 }
 
 static t_cmd	*parsing(char *command, int *status, t_cmd **cmd, char **envp)
@@ -60,8 +64,8 @@ static t_cmd	*parsing(char *command, int *status, t_cmd **cmd, char **envp)
 
 	save = ft_strdup(command);
 	nb_word = 0;
-	t_cmd = (*cmd) = ft_mlstadd((*cmd), status);
-	// t_cmd = *cmd;
+	*cmd = ft_mlstadd((*cmd), status);
+	t_cmd = *cmd;
 	while (!(*status) && *command != '\0')
 	{
 		find_next_word(&command, status, &nb_word, &t_cmd->cmd[nb_word]);
@@ -84,12 +88,21 @@ static t_cmd	*parsing(char *command, int *status, t_cmd **cmd, char **envp)
 	return (*cmd);
 }
 
+bool	_continue(t_cmd *cmd, char ***envp, int *status, int ctrl)
+{
+	return (ft_strlen(*cmd->cmd) && !exec_builtins(cmd, status, envp, MAIN)
+		&& ((ctrl != AND && ctrl != OR)
+			|| ((ctrl == AND && !*status)
+				|| (ctrl == OR && *status))));
+}
+
 static int	execute(char *command, int *status, char ***envp)
 {
-	t_cmd	*cmd = NULL;
+	t_cmd	*cmd;
 	t_cmd	*r_cmd;
-	int  	ctrl;
+	int		ctrl;
 
+	cmd = NULL;
 	ctrl = END;
 	*status = 0;
 	r_cmd = parsing(command, status, &cmd, *envp);
@@ -98,10 +111,7 @@ static int	execute(char *command, int *status, char ***envp)
 		if (ft_strlen(*cmd->cmd) && exec_pipe(cmd, status, *envp))
 			while (cmd->ctrl_op == PIPE)
 				cmd = cmd->next;
-		else if(ft_strlen(*cmd->cmd) && !exec_builtins(cmd, status, envp, MAIN)
-				&& ((ctrl != AND && ctrl != OR)
-				|| ((ctrl == AND && !*status)
-				|| (ctrl == OR && *status))))
+		else if (_continue(cmd, envp, status, ctrl))
 			exec_cmd(cmd, status, *envp, 0);
 		ctrl = cmd->ctrl_op;
 		cmd = cmd->next;
@@ -116,28 +126,28 @@ void	minishell_loop(int *status, char ***envp)
 {
 	char	*command;
 
-		while (42)
-		{
-			printf("status = %d\n", *status);
-			signal(SIGINT, handle_prompt);
-			signal(SIGQUIT, SIG_IGN);
-			command = readline("minishell %");
-			if (!command)
-				exit(exit_free_envp(envp));
-			signal(SIGINT, SIG_IGN);
-			printf(">>> %s\n", command);
-			if (*command != '\0')
-				execute(command, status, envp);
-			else
-				add_history(command);			//TODO Why?
-			free(command);
-		}
+	while (42)
+	{
+		printf("status = %d\n", *status);
+		signal(SIGINT, handle_prompt);
+		signal(SIGQUIT, SIG_IGN);
+		command = readline("minishell %");
+		if (!command)
+			exit(exit_free_envp(envp));
+		signal(SIGINT, SIG_IGN);
+		printf(">>> %s\n", command);
+		if (*command != '\0')
+			execute(command, status, envp);
+		else
+			add_history(command);
+		free(command);
+	}
 }
 
-int		main(int argc, char **argv, char **envp)
+int	main(int argc, char **argv, char **envp)
 {
 	int		status;
-	
+
 	status = 0;
 	if (argc == 1)
 	{
@@ -148,7 +158,7 @@ int		main(int argc, char **argv, char **envp)
 	else if (argc > 1 && ft_strncmp(argv[1], "-c", 2) == 0)
 		exit(execute(argv[2], &status, &envp));
 	dprintf(2, "minshell: %s: invalid option\n"
-	"Usage: minishell [option] [command]\n"
-	"Shell option :\n\t-c command\n", argv[1]);
+		"Usage: minishell [option] [command]\n"
+		"Shell option :\n\t-c command\n", argv[1]);
 	exit(2);
 }
