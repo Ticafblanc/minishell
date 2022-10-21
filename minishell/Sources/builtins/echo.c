f0ef4b65a906aa2ea5ebff0e0de582f896f9b2de
tonted <yddetco@gmail.com>
2022-10-21 23:18:12 +0200

    :bug: add remove quote in echo builtin

--- minishell/Sources/builtins/echo.c
+++ minishell/Sources/builtins/echo.c
@@ -6,7 +6,7 @@
 /*   By: tonted <tonted@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/11 09:10:51 by tonted            #+#    #+#             */
-/*   Updated: 2022/10/21 22:56:54 by tonted           ###   ########.fr       */
+/*   Updated: 2022/10/21 23:16:50 by tonted           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,7 +41,7 @@ int	exec_echo(t_cmd *cmd)
 		i++;
 	while (cmd->cmd[i])
 	{
-		write(1, cmd->cmd[i], ft_str_len(cmd->cmd[i]));
+		write(1, remove_quote(cmd->cmd[i]), ft_str_len(cmd->cmd[i]));
 		if (cmd->cmd[++i])
 			write(1, " ", 1);
 	}
