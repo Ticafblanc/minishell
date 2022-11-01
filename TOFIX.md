f5e9a357fc6891f27dea99ac7108df1e6b36b965
tonted <yddetco@gmail.com>
2022-11-01 07:44:34 +0100

    ➕ TODO after correction check

--- TOFIX.md
+++ TOFIX.md
@@ -9,3 +9,9 @@
 - [ ] echo $PATH
 - [ ] echo test>>file
 - [ ] echo test >> file
+
+# Correction
+- [ ] exit avec argument
+- [ ] donner une path absolue sans faire cd devant
+- [ ] cat Makefile | wc : reste stuck dans le cat
+- [ ] [bad command] | wc : reste stuck dans le child process
--- minishell/Sources/builtins/exit.c
+++ minishell/Sources/builtins/exit.c
@@ -6,12 +6,13 @@
 /*   By: tonted <tonted@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/11 09:11:19 by tonted            #+#    #+#             */
-/*   Updated: 2022/09/13 07:22:40 by tonted           ###   ########.fr       */
+/*   Updated: 2022/10/31 19:02:48 by tonted           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+// TODO manage arguments
 void	exec_exit(int process, char ***envp)
 {
 	if (process == MAIN)
