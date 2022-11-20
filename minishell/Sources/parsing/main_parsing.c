3d059c05bd8fa694a96191d4038568bbb48dd010
tblanco <tblanco@student.42quebec.com>
2022-11-20 13:46:37 -0500

    :bug: Improvement of get_sequl with subprocess by Mathis

--- minishell/Sources/parsing/main_parsing.c
+++ minishell/Sources/parsing/main_parsing.c
@@ -6,66 +6,54 @@
 /*   By: tblanco <tblanco@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/26 21:37:46 by tonted            #+#    #+#             */
-/*   Updated: 2022/11/20 13:02:21 by tblanco          ###   ########.fr       */
+/*   Updated: 2022/11/20 13:44:25 by tblanco          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	init_link(t_cmd *new)
-{
-	*(new->cmd) = NULL;
-	new->ctrl_op = END;
-	new->path = NULL;
-	new->infile = STDIN_FILENO;
-	new->outfile = STDOUT_FILENO;
-	new->fd[STDIN_FILENO] = STDIN_FILENO;
-	new->fd[STDOUT_FILENO] = STDOUT_FILENO;
-	new->flag = 0x0;
-	new->next = NULL;
-	new->pid = -2;
-}
-
-t_cmd	*ft_mlstadd(t_cmd *cmd)
-{
-	t_cmd	*new;
-
-	new = (t_cmd *)malloc(sizeof(t_cmd));
-	if (new)
-	{
-		new->cmd = (char **)ft_calloc(20, sizeof(char *));
-		if (new->cmd)
-		{
-			init_link(new);
-			if (cmd)
-				cmd->next = new;
-			return (new);
-		}
-		free(new);
-	}
-	set_status(errno);
-	perror("minishell:");
-	return (NULL);
-}
-
-static void	get_sequel(char **save, t_cmd *cmd, char **envp)
+static void	sequel_child(int fd[2])
 {
 	char	*sequel;
-	char	*tmp;
 
-	signal(SIGINT, handle_prompt);
-	signal(SIGQUIT, SIG_IGN);
-	tmp = *save;
+	signal(SIGINT, handle_exec);
+	close(fd[0]);
 	sequel = readline("> ");
 	if (!sequel)
 	{
 		dprintf(2, "minishell: syntax error: unexpected end of file\n");
-		set_status(TOKENERR);
-		return ;
+		exit (TOKENERR);
 	}
-	*save = ft_strjoin(*save, sequel);
-	free(tmp);
-	parsing_loop(&sequel, cmd, envp, save);
+	ft_putstr_fd(sequel, fd[1]);
+	free_null(sequel);
+	close (fd[1]);
+	exit (EXIT_SUCCESS);
+}
+
+static void	get_sequel(char **save, t_cmd *cmd, char **envp)
+{
+	int		fd[2];
+	char	*sequel;
+	char	*tmp;
+	pid_t	pid;
+
+	tmp = *save;
+	if (pipe(fd) != -1)
+	{
+		pid = fork();
+		if (pid != -1)
+		{
+			if (!pid)
+				sequel_child(fd);
+			waitpid(pid, get_status(), 0);
+		}
+		close(fd[1]);
+		sequel = get_next_line(fd[STDIN_FILENO]);
+		close(fd[0]);
+		*save = ft_strjoin(*save, sequel);
+		free(tmp);
+		parsing_loop(&sequel, cmd, envp, save);
+	}
 }
 
 void	parsing_loop(char **command, t_cmd *t_cmd, char **envp, char **save)
