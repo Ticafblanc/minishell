32967b60592091cc850d1ee759d23baf3b6ac340
tonted <yddetco@gmail.com>
2022-11-24 01:06:51 -0500

    :recycle: Refactor Exit and add free!

--- minishell/Sources/builtins/exit.c
+++ minishell/Sources/builtins/exit.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   exit.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tblanco <tblanco@student.42.fr>            +#+  +:+       +#+        */
+/*   By: tonted <tonted@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/11 09:11:19 by tonted            #+#    #+#             */
-/*   Updated: 2022/11/23 21:13:46 by tblanco          ###   ########.fr       */
+/*   Updated: 2022/11/24 00:48:25 by tonted           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,29 +27,24 @@ int	exec_exit(int process, char ***envp, t_cmd *cmd)
 
 	if (process == MAIN)
 		printf("exit\n");
-	if (cmd->cmd[1] && !is_number(cmd->cmd[1]))
-	{
-		dprintf (2, "minishell: exit: %s: numeric argument required\n",
-			cmd->cmd[1]);
-		ft_free_pp((void **)*envp);
-		free_next_cmds(cmd->begin);
-		exit(EXIT_FAILURE);
-	}
-	if (cmd->cmd[1] && cmd->cmd[2])
+	if (cmd->cmd[2] && is_number(cmd->cmd[1]))
 	{
 		dprintf (2, "minishell: exit: too many arguments\n");
 		set_status(1);
 		return (EXIT_FAILURE);
 	}
-	if (cmd->cmd[1])
+	if (cmd->cmd[1] && !is_number(cmd->cmd[1]))
 	{
-		status = ft_atoi(cmd->cmd[1]);
-		ft_free_pp((void **)*envp);
-		free_next_cmds(cmd->begin);
-		exit(status);
+		dprintf (2, "minishell: exit: %s: numeric argument required\n",
+			cmd->cmd[1]);
+		status = EXIT_FAILURE;
 	}
+	else if (cmd->cmd[1])
+		status = ft_atoi(cmd->cmd[1]);
+	else
+		status = 0;
 	ft_free_pp((void **)*envp);
 	free_next_cmds(cmd->begin);
-	exit(EXIT_SUCCESS);
-	return (EXIT_FAILURE);
+	exit(status);
+	return (status);
 }
