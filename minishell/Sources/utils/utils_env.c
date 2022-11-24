3cb435d373d0276f3f9443d1714040b1a05f0c98
tonted <yddetco@gmail.com>
2022-11-24 01:23:06 -0500

    :recycle: refactor find_path!

--- minishell/Sources/utils/utils_env.c
+++ minishell/Sources/utils/utils_env.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   utils_env.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tblanco <tblanco@student.42.fr>            +#+  +:+       +#+        */
+/*   By: tonted <tonted@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/13 06:50:45 by tonted            #+#    #+#             */
-/*   Updated: 2022/11/23 22:08:47 by tblanco          ###   ########.fr       */
+/*   Updated: 2022/11/24 01:22:12 by tonted           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,19 +35,14 @@ void	put_envp(char *prefix, char **envp, int fd)
 		dprintf(fd, "%s%s\n", prefix, envp[i++]);
 }
 
-char	*find_path(char *cmd, char **envp)
+char	*get_path(char *cmd, int i_env, char **envp)
 {
-	char	**paths;
-	char	*path;
 	int		i;
+	char	*path;
+	char	**paths;
 
 	i = 0;
-	if (is_name_in_envp(envp, "PATH") == -1)
-		return (NULL);
-	while (envp[i] && ft_strnstr(envp[i], "PATH=", 5) == 0)
-		i++;
-	paths = ft_split(envp[i] + 5, ':');
-	i = 0;
+	paths = ft_split(get_value(envp[i_env]), ':');
 	cmd = ft_strjoin("/", cmd);
 	while (paths[i])
 	{
@@ -66,32 +61,30 @@ char	*find_path(char *cmd, char **envp)
 	return (NULL);
 }
 
+char	*find_path(char *cmd, char **envp)
+{
+	char	*path;
+	int		i_env;
+
+	i_env = is_name_in_envp(envp, "PATH");
+	if (i_env == -1)
+		return (NULL);
+	path = get_path(cmd, i_env, envp);
+	if (path)
+		return (path);
+	return (NULL);
+}
+
 char	*find_path_child(char *cmd, char **envp)
 {
-	char	**paths;
 	char	*path;
-	int		i;
+	int		i_env;
 
-	i = 0;
-	if (is_name_in_envp(envp, "PATH") == -1)
+	i_env = is_name_in_envp(envp, "PATH");
+	if (i_env == -1)
 		return (ft_strdup(cmd));
-	while (envp[i] && ft_strnstr(envp[i], "PATH=", 5) == 0)
-		i++;
-	paths = ft_split(envp[i] + 5, ':');
-	i = 0;
-	cmd = ft_strjoin("/", cmd);
-	while (paths[i])
-	{
-		path = ft_strjoin(paths[i], cmd);
-		if (access(path, X_OK) == 0)
-		{
-			free_null((void *)cmd);
-			ft_free_pp((void **)paths);
-			return (path);
-		}
-		free_null((void *)path);
-		i++;
-	}
-	ft_free_pp((void **)paths);
+	path = get_path(cmd, i_env, envp);
+	if (path)
+		return (path);
 	return (cmd);
 }
